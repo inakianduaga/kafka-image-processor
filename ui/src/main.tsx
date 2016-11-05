9a438e092dc377c53597fb5df02fb3191f009e02
Inaki Anduaga <inaki.anduaga@tado.com>
2016-11-05 12:56:00 +0100

    Added some client side stats, small html changes

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,7 +1,7 @@
 import xs, {Stream, MemoryStream} from 'xstream';
 import {VNode, CycleDOMEvent} from '@cycle/dom';
-import { DOMSource } from '@cycle/dom/xstream-typings';
-const { html } = require('snabbdom-jsx');
+import {DOMSource} from '@cycle/dom/xstream-typings';
+const {html} = require('snabbdom-jsx');
 
 export type Sources = {
   DOM: DOMSource,
@@ -12,24 +12,26 @@ export type Sinks = {
   WEBSOCKET: Stream<any>,
 }
 
+const defaultFrequency = 3;
+const maxUploadImages = 5;
+const imageSize = 100;
+
 export default function Main(sources: Sources): Sinks {
 
-  const defaultFrequency = 1;
-
   const frequencySelection$ = sources
-      .DOM
-      .select('#freqSelect')
-      .events('change')
-      .map(event => (event.target as HTMLInputElement).value)
-      .startWith( `${defaultFrequency}` );
+    .DOM
+    .select('#freqSelect')
+    .events('change')
+    .map(event => (event.target as HTMLInputElement).value)
+    .startWith(`${defaultFrequency}`);
 
   const imageFrequencyControl$ = frequencySelection$.map((frequency: any) =>
     <div className="col col-xs-12">
       <div className="well">
-        <h4>Choose Image Frequency</h4>
-        <input type="range" name="quantity" min="1" max="5" id="freqSelect" value={ frequency } />
+        <h4>Image Upload Frequency</h4>
+        <input type="range" name="quantity" min="1" max="5" id="freqSelect" value={ frequency }/>
         <label>
-          { frequency }
+          every { frequency }s
         </label>
       </div>
     </div>
@@ -37,12 +39,14 @@ export default function Main(sources: Sources): Sinks {
 
   const randomImageUrl = (): string => {
     const random = Math.floor(Math.random() * (1000 - 0 + 1)) + 0;
-    return `https://unsplash.it/150/150?image=${random}`;
+    return `https://unsplash.it/${imageSize}/${imageSize}?image=${random}`;
   };
 
   const imageClock$ = frequencySelection$.map((frequency: any) => xs.periodic(frequency * 1000)).flatten();
 
-  const imageUrls$ = imageClock$.map(() => randomImageUrl());
+  const imageClockCapped$ = imageClock$.take(maxUploadImages);
+
+  const imageUrls$ = imageClockCapped$.map(() => randomImageUrl());
 
   const cumulativeImageUrls$ = imageUrls$.fold((acc: string[], url: string) => acc.push(url) && acc, []);
 
@@ -50,45 +54,62 @@ export default function Main(sources: Sources): Sinks {
     <div>
       {
         urls.map(url =>
-            <img src={url} />
+          <img src={url} style={{ border: "1px solid #ddd" }} height={imageSize} width={imageSize}/>
         )
       }
     </div>
   );
 
   const imageGallery$ = imageTags$.map(tags =>
-      <div className="col col-xs-12">
-          <h4>Image Stream...</h4>
-          <div>
-            { tags }
-          </div>
+    <div className="col col-xs-12">
+      <h4>Image Stream...</h4>
+      <div>
+        { tags }
       </div>
+    </div>
   );
 
   // Websocket stats
-  const stats$ = sources.WEBSOCKET
-      .startWith('no message yet');
+  const serverResults$ = sources.WEBSOCKET
+    .startWith('no message yet');
 
-  const statsBlocks$ = stats$.map(stats =>
+  // Client stats
+  const uploadCount$ = imageUrls$.fold((count) => count + 1, 0);
+  const uploadPercentage$ = uploadCount$.map(count => 100 * count / maxUploadImages);
+
+  const clientStats$ = xs.combine(uploadCount$, uploadPercentage$)
+    .map(([uploadCount, uploadPercentage]) =>
       <div className="col col-xs-12">
-          <h4>Processing stats</h4>
-          <div>
-              { stats }
+        <h4>Processing stats</h4>
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            <h5>Client uploads</h5>
+            <p>
+              { uploadCount } / { maxUploadImages } | { uploadPercentage}%
+            </p>
           </div>
+        </div>
       </div>
   );
 
   const vdom$ = xs
-      .combine(imageFrequencyControl$, imageGallery$, statsBlocks$)
-      .map(([imageFrequencyControl, imageGallery, statsBlocks]) =>
+    .combine(imageFrequencyControl$, imageGallery$, clientStats$)
+    .map(([imageFrequencyControl, imageGallery, clientStats]) =>
       <div className="container-fluid">
         <div className="row">
-          { imageFrequencyControl }
-          { imageGallery }
-          { statsBlocks }
+          <div className="col-xs-6 col-md-6">
+            <div className="row">
+              { imageFrequencyControl }
+              { clientStats }
+              { imageGallery }
+            </div>
+          </div>
+          <div className="col-xs-6 col-md-6">
+            HERE GO SERVER SIDE RESULTS
+          </div>
         </div>
       </div>
-  );
+    );
 
   return {
     DOM: vdom$,
