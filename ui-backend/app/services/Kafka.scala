61d2197756ceaff42e2ff5b51c27fd2b384380f7
Inaki Anduaga <inaki.anduaga@tado.com>
2016-12-31 12:17:38 +0100

    Closes #9, Closes #8: Fixed docker compose config so UI debugging tools work

--- ui-backend/app/services/Kafka.scala
+++ ui-backend/app/services/Kafka.scala
@@ -23,10 +23,11 @@ class Kafka @Inject() (configuration: play.api.Configuration) {
   }
 
   def send(value: String) = producer.send(KafkaProducerRecord(topic="Images.Urls", key=None, value))
-    .foreach(record => s"Pushed record to kafka: ${record.toString}")
 
   // Test producer
-  send("http://www.some.com")
+  send("http://www.some.com").onSuccess {
+    case t => println(s"Sent successfully ${t.timestamp}")
+  }
 
   // HACK: Save instance into object companion for retrieving it without needing dependency injection
   Kafka.setInstance(this)
