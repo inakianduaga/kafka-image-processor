a83f1ed996c9858e4687836e4500e4af5285a1d6
Inaki Anduaga <inaki.anduaga@tado.com>
2017-02-19 21:53:25 +0100

    Added more filters

--- processor/src/main/scala/com/inakianduaga/models/ImageRequest2.scala
+++ processor/src/main/scala/com/inakianduaga/models/ImageRequest2.scala
@@ -4,6 +4,7 @@ import com.sksamuel.{scrimage => ImgLib}
 import org.apache.avro.generic.GenericData.EnumSymbol
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.util.Utf8
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class ImageRequest2(url: String, id: String, filter: Filter) {}
 
@@ -17,9 +18,20 @@ object ImageRequest2 {
         case FilterGreyscale.name => FilterGreyscale
         case FilterChrome.name => FilterChrome
         case FilterHalftone.name => FilterHalftone
+        case BlurFilter.name => BlurFilter
+        case DiffuseFilter.name => DiffuseFilter
+        case ContourFilter.name => ContourFilter
+        case EdgeFilter.name => EdgeFilter
+        case OilFilter.name => OilFilter
+        case RobertsFilter.name => RobertsFilter
+        case SummerFilter.name => SummerFilter
+        case TritoneFilter.name => TritoneFilter
+        case SolarizeFilter.name => SolarizeFilter
+        case TelevisionFilter.name => TelevisionFilter
+        case VintageFilter.name => VintageFilter
+        case InvertFilter.name => InvertFilter
       }
     )
-
 }
 
 sealed trait Filter {
@@ -39,6 +51,67 @@ case object FilterHalftone extends Filter {
   val name = "HALFTONE"
   override val filter = ImgLib.filter.ColorHalftoneFilter()
 }
+
+case object BlurFilter extends Filter {
+  val name = "BLUR"
+  override val filter = ImgLib.filter.BlurFilter
+}
+
+case object DiffuseFilter extends Filter {
+  val name = "DIFFUSE"
+  override val filter = ImgLib.filter.DiffuseFilter(3)
+}
+
+case object ContourFilter extends Filter {
+  val name = "CONTOUR"
+  override val filter = ImgLib.filter.ContourFilter()
+}
+
+case object EdgeFilter extends Filter {
+  val name = "EDGE"
+  override val filter = ImgLib.filter.EdgeFilter
+}
+
+case object OilFilter extends Filter {
+  val name = "OIL"
+  override val filter = ImgLib.filter.OilFilter(2, 4)
+}
+
+case object RobertsFilter extends Filter {
+  val name = "ROBERTS"
+  override val filter = ImgLib.filter.RobertsFilter
+}
+
+case object SummerFilter extends Filter {
+  val name = "SUMMER"
+  override val filter = ImgLib.filter.SummerFilter(true)
+}
+
+case object TritoneFilter extends Filter {
+  val name = "TRITONE"
+  override val filter = ImgLib.filter.TritoneFilter(150, 80, 50)
+}
+
+case object SolarizeFilter extends Filter {
+  val name = "SOLARIZE"
+  override val filter = ImgLib.filter.SolarizeFilter
+}
+
+case object TelevisionFilter extends Filter {
+  val name = "TELEVISION"
+  override val filter = ImgLib.filter.TelevisionFilter
+}
+
+case object VintageFilter extends Filter {
+  val name = "VINTAGE"
+  override val filter = ImgLib.filter.VignetteFilter()
+}
+
+case object InvertFilter extends Filter {
+  val name = "INVERT"
+  override val filter = ImgLib.filter.InvertFilter
+}
+
 
 
 
