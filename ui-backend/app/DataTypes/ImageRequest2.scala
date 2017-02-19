48bc8a8bf6f65dcef982ae3d267f962a2b69c496
Inaki Anduaga <inaki.anduaga@tado.com>
2017-02-19 13:04:43 +0100

    #15: Fixed parsing json on the ui backend

--- ui-backend/app/DataTypes/ImageRequest2.scala
+++ ui-backend/app/DataTypes/ImageRequest2.scala
@@ -3,7 +3,7 @@ package DataTypes
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericData.Record
 
-case class ImageRequest2(url: String, id: String, filter: String) extends AvroRecordConvertible {
+case class ImageRequest2(url: String, filter: String, id: String) extends AvroRecordConvertible {
   def toAvroRecord(avroSchema: Schema): Record = {
     val avroRecord = new Record(avroSchema)
     avroRecord.put("url", url)
--- ui-backend/app/services/WebSocketActor.scala
+++ ui-backend/app/services/WebSocketActor.scala
@@ -15,35 +15,43 @@ class WebSocketActor (out: ActorRef) extends Actor {
   val actorPath = out.path.toString
 
   def receive = {
-    case msg: String =>
-      println(msg)
     // Catch all messages as generic Json and handle parsing of different potential types inside
     case msg: JsValue =>
       selectVersionFromJson(msg)
         .fold[Unit](
-        image =>
-          getInstance()
-            .send(image.get, "./app/schemas/imageRequest.avsc")
-            .onSuccess{ case _ => println(s"V1: Pushed url to kafka ${image.get.url} in Avro format") }
-        ,
-        image =>
-          getInstance()
-            .send(image.get, "./app/schemas/imageRequest2.avsc")
-            .onSuccess{ case _ => println(s"V2: Pushed url & filter to kafka ${image.get.url} in Avro format") }
+          image =>
+            getInstance()
+              .send(image, "./app/schemas/imageRequest.avsc")
+              .onSuccess{ case _ => println(s"V1: Pushed url to kafka ${image.url} in Avro format") }
+          ,
+          image =>
+            getInstance()
+              .send(image, "./app/schemas/imageRequest2.avsc")
+              .onSuccess{ case _ => println(s"V2: Pushed url & filter to kafka ${image.url} in Avro format") }
         )
     case _ =>
-      println(s"Uncaught message type")
+      println(s"Error: Unsupported message type")
   }
 
   /**
     * The schema version to be used based on the client JSON payload
     */
-  private def selectVersionFromJson(msg: JsValue): Either[JsResult[ImageRequest], JsResult[ImageRequest2]] =
+  private def selectVersionFromJson(msg: JsValue): Either[ImageRequest, ImageRequest2] =
     if ((msg \ "filter").asOpt[String].isEmpty)
-      Left(Json.fromJson(msg)(Json.reads[ImageRequest]).map(_.copy(id = actorPath)))
+      Left(
+        ImageRequest(
+          url = (msg \ "url").as[String],
+          id = actorPath
+        )
+      )
     else
-      Right(Json.fromJson(msg)(Json.reads[ImageRequest2]).map(_.copy(id = actorPath)))
-
+      Right(
+        ImageRequest2(
+          url = (msg \ "url").as[String],
+          filter = (msg \ "filter").as[String],
+          id = actorPath
+        )
+      )
 }
 
 object WebSocketActor {
